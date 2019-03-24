8b2219b5ded8d36b3fc88379214f87a4fac0b336
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-03-24 11:24:21 -0700

    appended post header and text body to lists and cleaned up code

--- scraper.py
+++ scraper.py
@@ -2,42 +2,65 @@
 
 import praw #pkg that connects to reddit api
 import pandas as pd #pkg that handles/formats the data
-import datetime as dt
 
 #using python reddit api wrapper to make requests
-reddit = praw.Reddit(user_agent='Comment Extraction (by /u/USERNAME)',client_id='QvAdTaUsIGkukg',client_secret="6ZT2FBooftzF3wA3I7d5XEzIayI",username='ebonnecab', password='Makeschool1!')
+reddit = praw.Reddit(user_agent='project-1 (by /u/ebonnecab)',client_id='QvAdTaUsIGkukg', client_secret="6ZT2FBooftzF3wA3I7d5XEzIayI", username='ebonnecab', password='Makeschool1!')
 
-#accessing prison reform subreddit
+# accessing prison reform subreddit
 subreddit = reddit.subreddit('prisonreform')
 
-#accessing hot subreddit
-python_subreddit = subreddit.hot(limit=1000)
 
-#accessing top-500 submissions in r/prisonreform
-top_subreddit = subreddit.top(limit=500)
+#accessing hot category of subreddit
+hot_subreddit = subreddit.hot(limit=2)
 
 #sample code that prints submission title and id of top submission
 # for submission in subreddit.top(limit=1):
 #     print(submission.title, submission.id)
 
-# created an empty dictionary of features
+# created an empty dictionary of features for top voted and hottest subreddit posts
 topics_dict = {"title": [],
-               "subreddit": [],
                "score": [],
-               "id": [], "url": [],
-               "comms_num": [],
+               "id": [], 
+               "url": [],
                "created": [],
                "body": []}
 
+comm_list = []
+header_list = []
+i = 0
 #iterating through chosen features and appending to dict
-for submission in python_subreddit:
+for submission in hot_subreddit:
     topics_dict["title"].append(submission.title)
-    topics_dict['subreddit'].append(submission.subreddit)
-    topics_dict["score"].append(submission.score)
     topics_dict["id"].append(submission.id)
-    topics_dict["url"].append(submission.url)
-    topics_dict["comms_num"].append(submission.num_comments)
-    topics_dict["created"].append(submission.created)
+    # topics_dict["url"].append(submission.url)
+    # topics_dict["comms_num"].append(submission.num_comments)
+    # topics_dict["comments"].append(submission.comments)
+    # topics_dict["created"].append(submission.created)
     topics_dict["body"].append(submission.selftext)
 
-print(topics_dict)
+    while topics_dict["body"]:
+        header_list.append(topics_dict["title"])
+        comment = topics_dict["body"].pop(0)
+        comm_list.append(comment)
+        t = []
+        t.extend(topics_dict["body"].replies)
+        while t:
+            header_list.append(topics_dict["title"])
+            reply = t.pop(0)
+            comm_list.append(reply)
+
+# print(topics_dict)
+print(header_list, comm_list)
+# for submissions in top_subreddit:
+#     topics_dict2["title"].append(submission.title)
+#     topics_dict2["score"].append(submission.score)
+#     topics_dict2["id"].append(submission.id)
+#     topics_dict2["url"].append(submission.url)
+#     topics_dict2["comms_num"].append(submission.num_comments)
+#     topics_dict2["created"].append(submission.created)
+#     topics_dict2["body"].append(submission.selftext)
+
+# dataframe = pd.DataFrame(topics_dict)
+# dataframe.columns = ['']
+
+# d = pd.read_csv(r'./v1.csv')
