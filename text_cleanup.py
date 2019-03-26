acfb46c3006a80153d1ce6197392798591d717bc
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-03-26 00:25:25 -0700

    text cleaned and prepped for analysis

--- text_cleanup.py
+++ text_cleanup.py
@@ -8,6 +8,20 @@ file.close()
 from nltk import word_tokenize
 tokens = word_tokenize(text)
 
+#convert tokens to lowercase
+tokens = [word.lower() for word in tokens]
+
+#removes punctuation from each word
+import string
+table = str.maketrans('', '', string.punctuation)
+stripped = [w.translate(table) for w in tokens]
+
 #remove non-alphabetic tokens
-words = [word for word in tokens if word.isalpha()]
+words = [word for word in stripped if word.isalpha()]
+
+#remove stop words
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+words = [w for w in words if not w in stop_words]
+
 print(words[:100])
