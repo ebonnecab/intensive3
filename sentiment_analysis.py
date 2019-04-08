35f840b4ca2a7f555bcab45e645a87f81b13bac9
Ebonne Cabarrus <cabarrus.ebonne@gmail.com>
2019-04-08 12:19:26 -0700

    refactored sentiment analysis file

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,37 +1,50 @@
+#! usr/bin/env python3
 
 #split text into word tokens
 from nltk import word_tokenize
 import string
 from nltk.corpus import stopwords
-   
+
+def read_file(file):
+    with open(file) as f:  # access file
+        text = f.read()
+    
+    return text
+
+def get_tokens(text):
+    #split text into word tokens
+    tokens = word_tokenize(text)
+    #converts tokens to lowercase
+    tokens = [word.lower() for word in tokens]
+    #removes punctuation from each word
+    table = str.maketrans('', '', string.punctuation) 
+    stripped = [w.translate(table) for w in tokens]
+    #remove non-alphabetic tokens
+    words = [word for word in stripped if word.isalpha()]
+
+    return words
+
+def stop_words(words):
+    stop_words = set(stopwords.words('english'))
+    #remove stop words from corpus
+    clean_words = [w for w in words if not w in stop_words]
+    
+    return clean_words
+
 #import textblob library for simple NLP tasks
 from textblob import TextBlob
 
-#function for sentiment analysis
-def sentiment():
-    #load data file
-    filename = './v1.csv'
-    file = open(filename, 'rt')
-    text = file.read()
-    file.close()
-
-    tokens = word_tokenize(text) #split text into word tokens
-    tokens = [word.lower() for word in tokens] #converts tokens to lowercase   
-    table = str.maketrans('', '', string.punctuation) #removes punctuation from each word
-    stripped = [w.translate(table) for w in tokens]
-    words = [word for word in stripped if word.isalpha()] #remove non-alphabetic tokens
-    stop_words = set(stopwords.words('english'))
-    words = [w for w in words if not w in stop_words] #remove stop words from corpus
-    word_blob = ' '.join(words) #joined list to string to use text blob library
-
+def get_sentiment(clean_words):
+     #joined list to string to use text blob library
+    word_blob = ' '.join(clean_words)
     blob = TextBlob(word_blob) #create blob object
-
-    for word in blob.split(): #iterate over each word in string
+    for word in blob.split():
         print(word)
-        analysis = TextBlob(word) 
-        print(analysis.sentiment) # determines polarity and subjectivity scores of each word
-        
-        #categorizing words based upon sentiment value between -1 and 1
+        analysis = TextBlob(word)
+        # determines polarity and subjectivity scores of each word
+        print(analysis.sentiment)
+
+    #categorizing words based upon sentiment value between -1 and 1
         if analysis.sentiment[0]>0: 
             print('Positive')
         elif analysis.sentiment[0]<0:
@@ -40,8 +53,9 @@ def sentiment():
             print ('Neutral')
         
 
-
-if __name__ == '__main__': 
-    sentiment()
-
-
+if __name__ == '__main__':
+    text = read_file('corpus.txt')
+    tokens = get_tokens(text)
+    clean_tokens = stop_words(tokens)
+    final_analysis = get_sentiment(clean_tokens)
+    print(final_analysis)
